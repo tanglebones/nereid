d780b37e2309ffd9f3aa3adae7a19e25ca103e02
Cliff Hammerschmidt <chammerschmidt@nicolawealth.com>
2022-04-01 12:55:21 -0700

    fix exportActions. First arg is props?

--- lib/webreactcore/src/export_actions.ts
+++ lib/webreactcore/src/export_actions.ts
@@ -1,7 +1,7 @@
 import {actionsType} from "./react.type";
 
 export const exportActions = (actions: actionsType | undefined, given: actionsType) => {
-  if (actions) {
+  if (actions && !Object.isFrozen(actions)) {
     Object.assign(actions, given);
   }
 };
--- web/example/src/frame.tsx
+++ web/example/src/frame.tsx
@@ -1,15 +1,25 @@
 import React from "react";
-import {BrowserRouter, NavLink} from "react-router-dom";
+import {BrowserRouter, NavLink, Routes, Route} from "react-router-dom";
+import {Counter} from "@nereid/webreactexample";
 
 export const Frame = () => {
   return <BrowserRouter>
     <div id="frame">
       <h1>Example</h1>
       <nav>
-        <NavLink to="/">Home</NavLink> |{" "}
-        <NavLink to="/test">Test</NavLink> |{" "}
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/test">Test</NavLink>
         <NavLink to="/counter">Counter</NavLink>
       </nav>
+      <main>
+        <span>xxx</span>
+        <Routes>
+          <Route path="*" element={<span>test</span>}/>
+          <Route path="test" element={<span>test</span>}/>
+          <Route path="counter" element={<Counter/>}/>
+        </Routes>
+        <Counter/>
+      </main>
     </div>
   </BrowserRouter>
 };
